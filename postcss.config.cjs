6b76241dde62731b6ec6a62eeabb93aa2cabe339
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2022-04-27 02:39:02 +0800

    2-10 items now supported

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,17 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const postcssFor = require("postcss-for");
+const postcssTrig = require("postcss-simple-trig");
 
 const config = {
-  plugins: [
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
-    autoprefixer,
-  ],
+	plugins: [
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		postcssFor,
+		tailwindcss(),
+		//But others, like autoprefixer, need to run after,
+		autoprefixer,
+		postcssTrig,
+	],
 };
 
 module.exports = config;
--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,77 @@
 <script lang="ts">
   import {tick} from 'svelte';
-  import {scale} from 'svelte/transition';
+  import {scale,slide,fly} from 'svelte/transition';
   let rotate = false;
   let final = false;
   let adjust = false;
-  let options = ['1', '2', '3', '4'];
+  const allOptions = ['🍣', '🐟', '🦀', '🍕', '🥗', '🦞', '🦪', '🦐', '🦑', '🌶️']
+  let options = ['🍣', '🐟', '🦀', '🍕', '🥗', '🦞', '🦪', '🦐', '🦑', '🌶️'];
+  //['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
   let disabled = false;
+
   $: n = options.length;
+  $: selection = getRandomInt(1,n);
+  $: if(selection) {
+    reset();
+  }
+  $: if(n) {
+    reset();
+  }
 
   const turns = 5.25;
 
   function spinWheel() {
     rotate = true;
     disabled = true;
-    setTimeout(()=>{adjust=true;},1000);
-    setTimeout(()=>{disabled=false; final=true;},4000);
+    setTimeout(()=>{adjust=true;},500);
+    setTimeout(()=>{disabled=false; final=true;},1000);
+  }
+  function reset() {
+    rotate = false;
+    final = false;
+    adjust = false;
+  }
+  function getRandomInt(min: number, max: number): number { 
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 </script>
 
 <main>
-  <section class="grid justify-items-center relative">
-    <ul class:rotate>
-      {#each options as option}
-        <li class:adjust>{option}</li>
+  <section class="grid justify-items-center relative mt-8">
+    {#key options}
+    <ul class:rotate class="rotate-{selection}-{n} background-{n}"
+      in:scale={{start:1.1, opacity:1}}
+    >
+      {#each options as option,i}
+        <li 
+          class="position-{i+1}-{n}"
+          class:text-6xl={n < 5}
+          class:text-5xl={n >= 5 && n < 8}
+          class:text-4xl={n>=8}
+          on:click={()=>{
+            if (!(rotate&&!final) && n>=3){
+              options = [...options.slice(0,i), ...options.slice(i+1)];
+            }
+          }}
+        >
+          <span class:adjust>{option}</span>
+        </li>
       {/each}
     </ul>
+    {/key}
     {#if final}
-    <div class="absolute wheel-center p-2" transition:scale>
-      Hello
+    <div 
+      class="absolute wheel-center p-2"          
+      class:text-6xl={n < 5}
+      class:text-5xl={n >= 5 && n < 8}
+      class:text-4xl={n>=8}
+      transition:scale
+    >
+      {options[selection-1]}
     </div>
     {/if}
+    <div class="absolute arrow-head">
+    </div>
   </section>
   <section class="grid m-4">
     <div>
@@ -42,16 +84,33 @@
             spinWheel();
             return
           }
-          rotate = false;
-          final = false;
-          adjust = false;
+          selection = getRandomInt(1,n);
           await tick();
-          setTimeout(spinWheel,500)
+          setTimeout(spinWheel,250)
         }}
       >
         Spin the wheel
       </button>
     </div>
+    {#if final}
+      <div class="p-4 flex gap-4 items-center justify-center" in:slide={{delay: 400}}>
+        <div class="text-6xl">
+          {options[selection-1]}
+        </div>
+        <div
+          in:fly={{delay: 800, x: -50}}
+        >
+          Text
+        </div>
+      </div>
+    {/if}
+
+    <button class="btn" on:click={()=>{options=allOptions.slice(0,n-1)}}>
+      -
+    </button>
+    <button class="btn" on:click={()=>{options=allOptions.slice(0,n+1)}}>
+      +
+    </button>
   </section>
 </main>
 
@@ -60,61 +119,41 @@
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-    width: 100vw;
-    box-sizing: border-box;
+    width: 99vw;
     min-height: 100vh;
+    overflow-x: hidden;
   }
+
   ul { 
 		list-style: none;
 		position: relative;
 		transform-origin: center;
-		width: max(80vh, 80vh);
+		width: var(--spinner-width);
 		aspect-ratio: 1/1;
 		display: grid;
 		place-items: center;
-		background: conic-gradient(
-			hsla(0, 100%, 50%, 0.5) 45deg,
-			hsla(63, 100%, 50%, 0.5) 45deg,
-			hsla(63, 100%, 50%, 0.5) 135deg,
-			hsla(127, 100%, 50%, 0.5) 135deg,
-			hsla(127, 100%, 50%, 0.5) 225deg,
-			hsla(191, 100%, 50%, 0.5) 225deg,
-			hsla(191, 100%, 50%, 0.5) 315deg,
-			hsla(0, 100%, 50%, 0.5) 315deg
-		);
 		box-sizing: border-box;
 		border-radius: 50%;
-    border: 1px solid black;
+    border: 4px solid saddlebrown;
 	}
 	
 	.rotate{
-		animation: rotate 4s ease-in-out forwards;
+		animation: rotate var(--spin-time) ease-in-out forwards;
 	}
 	.adjust{
-		transform: rotate(-.25turn);
+		transform: rotate(calc(0turn - var(--rotate-angle)));
 	}
 	
 	li {
-		width: 30px; height: 30px; display: grid; place-items: center; position: absolute;
+		display: grid; place-items: center; position: absolute; aspect-ratio: 1/1; padding: 0.25em;
 		box-sizing: border-box; 
 		border-radius: 50%;
 	}
-  li:nth-child(1) {
-		right: 0.5em;
-	}
-	li:nth-child(2) {
-		bottom: 0.5em;
-	}
-	li:nth-child(3) {
-		left: 0.5em;
-	}
-	li:nth-child(4) {
-		top: 0.5em;
-	}
+
 
   @keyframes rotate {
 		100% {
-			transform: rotate(4.25turn);
+			transform: rotate(calc(4turn + var(--rotate-angle)));
 		}
 	}
 
@@ -122,8 +161,44 @@
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background: white;
     border: 1px solid black;
-    box-sizing: border-box;
+    background: #eeeeeeff;
+    animation-name: blinkingBG;
+    animation-duration: 0.3s;
+    animation-iteration-count: 5;
+    animation-timing-function: ease-in-out;
+
+  }
+
+  .arrow-head {
+    width: 0; 
+    height: 0; 
+    border-left:  calc(var(--spinner-radius) / 20) solid  transparent;
+    border-right: calc(var(--spinner-radius) / 20) solid transparent;
+    border-top:   calc(var(--spinner-radius) / 10) solid #555;
+    top: calc(var(--spinner-radius) / -10 + 0.25rem);
+  }
+
+  @keyframes blinkingBG {
+    0% {
+      background-color: red;
+      transform: translate(-50%, -50%) scale(1.05);
+    }
+    25% {
+      background-color: yellow;
+      transform: translate(-50%, -50%) scale(0.95) rotate(5deg);
+    }
+    50% {
+      background-color: green;
+      transform: translate(-50%, -50%) scale(1.05);
+    }
+    75% {
+      background-color: blue;
+      transform: translate(-50%, -50%) scale(0.95) rotate(-5deg);
+    }
+    100% {
+      background-color: violet;
+      transform: translate(-50%, -50%) scale(1.05);
+    }
   }
 </style>
