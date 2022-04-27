a07b15bafaa5a0fe6b9a0b8837009b1fe900a59d
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2022-04-27 14:39:23 +0800

    add to wheel option

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,41 @@
   import {scale,slide,fly,crossfade,fade} from 'svelte/transition';
   import {flip} from 'svelte/animate';
   const [send, receive] = crossfade({});
+  import {icons} from './icons';
 
   // spinner and button
+  let firstSpin = true;
   let rotate = false;
   let final = false;
+  let completed = false;
   let adjust = false;
   let disabled = false;
+
+  // new addition
+  let selectedIcon = 'sushi';
+  let name = ""
+  $: addDisabled = (name === "");
 
   // array
   let uid = 0;
   const initialOptions = [
-    { id: uid++,  inPlay: true, icon: '🍣', text: 'Sushi'},
-    { id: uid++,  inPlay: true, icon: '🐟', text: 'Fish'},
-    { id: uid++,  inPlay: true, icon: '🦀', text: 'Crab'},
-    { id: uid++,  inPlay: true, icon: '🍕', text: 'Pizza'},
-    { id: uid++,  inPlay: true, icon: '🥗', text: 'Vegetarian'},
-    { id: uid++,  inPlay: true, icon: '🦞', text: 'Lobster'},
-    { id: uid++,  inPlay: true, icon: '🦪', text: 'Oyster'},
+    { id: uid++,  inPlay: true, icon: '🍣', text: 'Kuro Maguro'},
+    { id: uid++,  inPlay: true, icon: '🍕', text: 'd.o.c.'},
+    { id: uid++,  inPlay: true, icon: '🥗', text: 'Elemen'},
+    { id: uid++,  inPlay: true, icon: '🍔', text: 'Black Tap'},
+    { id: uid++,  inPlay: true, icon: icons.tomato, text: 'Hai Di Lao'},
+    { id: uid++,  inPlay: false, icon: '🦀', text: 'Crab'},
+    { id: uid++,  inPlay: false, icon: '🦪', text: 'Oyster'},
+    { id: uid++,  inPlay: false, icon: '🐟', text: 'Fish'},
+    { id: uid++,  inPlay: false, icon: '🦞', text: 'Lobster'},
     { id: uid++,  inPlay: false, icon: '🦐', text: 'Prawn'},
     { id: uid++,  inPlay: false, icon: '🦑', text: 'Octopus'},
-    { id: uid++,  inPlay: false, icon: '🌶️', text: 'Mala'},
-    { id: uid++,  inPlay: false, icon: '🍔', text: 'Burger'},
+    { id: uid++,  inPlay: false, icon: '🌶️', text: 'Xiao Long Kan'},
+    { id: uid++,  inPlay: false, icon: icons.cook, text: 'Bread Street Kitchen'},
   ];
-  let options = [...initialOptions];
+  let options = initialOptions.map(item=>{
+    return {...item}
+  });
 
   $: n = options.filter(t=>t.inPlay).length;
   $: selection = getRandomInt(1,n);
@@ -33,19 +45,23 @@
     resetSpinner();
   }
   $: if(n) {
-    resetSpinner();
+    resetSpinner(true);
   }
 
   function spinWheel() {
+    firstSpin = false;
     rotate = true;
     disabled = true;
     setTimeout(()=>{adjust=true;},500);
     setTimeout(()=>{disabled=false; final=true;},1000);
+    setTimeout(()=>{completed=true;},3500);
   }
-  function resetSpinner() {
+  function resetSpinner(resetFirstSpin=false) {
+    firstSpin = resetFirstSpin ? true : firstSpin;
     rotate = false;
     final = false;
     adjust = false;
+    completed = false;
   }
   function getRandomInt(min: number, max: number): number { 
 	  return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -54,7 +70,7 @@
 
 <main>
   <!--Spinner-->
-  <section class="grid justify-items-center relative mt-8">
+  <section class="grid justify-items-center relative">
     <ul class:rotate class="rotate-{selection}-{n} background-{n}"
       in:scale={{start:1.1, opacity:1}}
     >
@@ -78,29 +94,12 @@
         </li>
       {/each}
     </ul>
-    <!--Outcome-->
-    {#if final}
-      <div 
-        class="absolute wheel-center winner p-2"          
-        class:text-6xl={n < 5}
-        class:text-5xl={n >= 5 && n < 8}
-        class:text-4xl={n>=8}
-        transition:scale
-      >
-        {options.filter(t=>t.inPlay)[selection-1].icon}
-      </div>
-    {/if}
-    <div class="absolute arrow-head">
-    </div>
-  </section>
-  <!--Button and detailed outcome-->
-  <section class="grid m-4">
-    <!--Button-->
-    <div>
+    <!--Center of wheel-->
+    {#if firstSpin && !rotate}
       <button 
-        class="btn btn-primary"
-        class:disabled
-        {disabled}
+        class="btn-primary spinner-button absolute wheel-center"
+        in:scale
+        out:fade
         on:click={async ()=>{
           if (!rotate){
             spinWheel();
@@ -114,11 +113,44 @@
       >
         Spin the wheel
       </button>
+    {:else if final && !completed}
+      <div 
+        class="absolute wheel-center winner p-2 bg-yellow-100"          
+        class:text-6xl={n < 5}
+        class:text-5xl={n >= 5 && n < 8}
+        class:text-4xl={n>=8}
+        transition:scale
+      >
+        {options.filter(t=>t.inPlay)[selection-1].icon}
+      </div>
+    {:else if completed}
+          <button 
+        class="btn-primary spinner-button absolute wheel-center"
+        in:scale
+        out:fade
+        on:click={async ()=>{
+          if (!rotate){
+            spinWheel();
+            return
+          }
+          selection = getRandomInt(1,n);
+          resetSpinner();
+          await tick();
+          setTimeout(spinWheel,250)
+        }}
+      >
+        Spin again
+      </button>
+    {/if}
+    <div class="absolute arrow-head">
     </div>
-    <!--Detailed outcome-->
+  </section>
+  <!--Button and detailed outcome-->
+  <section class="grid m-4">
     {#if final}
-      <div class="flex justify-center" in:slide={{delay: 400}}>
-        <div class="border border-black flex p-4 m-4 items-center justify-center gap-4 bg-yellow-100">
+    <!--Detailed outcome-->
+      <div class="flex justify-center" in:slide={{delay: 400}} out:slide>
+        <div class="border border-black flex p-4 m-4 items-center justify-center gap-4 bg-yellow-100 font-bold font-serif">
           <div class="text-6xl">
             {options.filter(t=>t.inPlay)[selection-1].icon}
           </div>
@@ -153,39 +185,61 @@
     {/each}
   </div>
   </section>
-  <!--Reset-->
-  <button 
-    class="btn" 
-    on:click={()=>{
-      uid = 0;
-      options = [  
-        { id: uid++,  inPlay: true, icon: '🍣', text: 'Sushi'},
-        { id: uid++,  inPlay: true, icon: '🐟', text: 'Fish'},
-        { id: uid++,  inPlay: true, icon: '🦀', text: 'Crab'},
-        { id: uid++,  inPlay: true, icon: '🍕', text: 'Pizza'},
-        { id: uid++,  inPlay: true, icon: '🥗', text: 'Vegetarian'},
-        { id: uid++,  inPlay: true, icon: '🦞', text: 'Lobster'},
-        { id: uid++,  inPlay: true, icon: '🦪', text: 'Oyster'},
-        { id: uid++,  inPlay: true, icon: '🦐', text: 'Prawn'},
-        { id: uid++,  inPlay: true, icon: '🦑', text: 'Octopus'},
-        { id: uid++,  inPlay: false, icon: '🌶️', text: 'Mala'},
-        { id: uid++,  inPlay: false, icon: '🍔', text: 'Burger'},
-      ];
-      resetSpinner();
-    }}
-  >
-      Reset
-  </button>
 </main>
+
+<section class="my-4 p-4 bg-gray-300 grid gap-4 justify-center">
+  <div class="flex justify-center">
+    <button 
+      class="btn btn-primary btn-sm"
+      class:btn-disabled={addDisabled}
+      on:click={()=>{
+        options = [...options, {id: uid++, inPlay: n<10, icon: icons[selectedIcon], text: name}];
+        name=""
+      }}
+    >
+      Add to {n>=10 ? 'options' : 'wheel'}
+    </button>
+  </div>
+  <div class="flex justify-center">
+    <input bind:value={name} type="text" placeholder="Name" class="input input-bordered w-full max-w-xs">
+  </div>
+  <div class="flex flex-wrap justify-center max-w-md">
+    {#each Object.keys(icons) as iconName}
+      <div
+        class:bg-primary={iconName===selectedIcon}
+        class="px-2 py-1 rounded-full cursor-pointer transition-colors duration-500"
+        on:click={()=>{selectedIcon=iconName}}
+      >
+        {icons[iconName]}
+      </div>
+    {/each}
+  </div>
+</section>
+
+<!--Reset-->
+<div class="flex justify-center pb-4">
+  <div>
+    <button 
+      class="btn btn-sm" 
+      on:click={()=>{
+        options = initialOptions.map(item=>{
+          return {...item}
+        });
+        resetSpinner(true);
+      }}
+    >
+        Reset
+    </button> 
+  </div>
+</div>
+  
 
 <style>
   main {
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-    width: 99vw;
-    min-height: 100vh;
-    overflow-x: hidden;
+    width: 100vw;
   }
 
   ul { 
@@ -226,13 +280,15 @@
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    transition-property: none;
+    transition-duration: 0s;
+    transition-timing-function: none;
   }
   .winner {
     animation-name: blinkingBG;
     animation-duration: 0.3s;
     animation-iteration-count: 5;
     animation-timing-function: ease-in-out;
-    background: #eeeeeeff;
     border: 1px solid black;
   }
 
